5624fe13fdd12bb7ab294839eaad743b8434ce2a
Michele-Versiglioni <micheleversiglioni07@gmail.com>
2026-03-09 16:29:01 +0100

    Modello Client Server UDP

---
+++ src/MainServer.java
@@ -0,0 +1,10 @@
+public class MainServer {
+
+    public static void main(String[] args) {
+
+        int port = 5000;
+
+        ServerUDP server = new ServerUDP(port);
+        server.start();
+    }
+}
